b24221abc8fc099eb4fa45cac09d7a96b452a46b
Niranjkv <vijayakumarniranj@gmail.com>
2025-12-16 21:10:34 +0530

    Initial commit for Medical Devices Inventory System

---
+++ src/main/java/com/healthcare/medical_devices_inventory/MedicalDevicesInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.medical_devices_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDevicesInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDevicesInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthcare/medical_devices_inventory/MedicalDevicesInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.medical_devices_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDevicesInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
