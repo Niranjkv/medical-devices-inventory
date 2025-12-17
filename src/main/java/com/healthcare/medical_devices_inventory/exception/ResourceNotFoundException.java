41845900999cfd17541e52678fd2a0943b4ab5ca
Niranjkv <vijayakumarniranj@gmail.com>
2025-12-17 12:22:36 +0530

    created a enum of DeviceStatus and DeviceStatusRecord is created and created exception file alone.

---
+++ src/main/java/com/healthcare/medical_devices_inventory/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.healthcare.medical_devices_inventory.exception;
+
+public class ResourceNotFoundException {
+    
+}
---
+++ src/main/java/com/healthcare/medical_devices_inventory/model/DeviceStatusRecord.java
@@ -0,0 +1,37 @@
+package com.healthcare.medical_devices_inventory.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeviceStatusRecord{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="device_id",referencedColumnName="id")
+    private Device device;
+
+    @Enumerated(EnumType.STRING)
+    private DeviceStatus currentStatus;
+
+    private LocalDate statusChangeDate;
+
+
+}
