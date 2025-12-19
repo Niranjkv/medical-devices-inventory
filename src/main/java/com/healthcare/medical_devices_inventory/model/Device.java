188ee54c661fc50de1dbcb01b61664c43c5f8db4
GokulC1205 <gokulc180@gmail.com>
2025-12-19 15:32:45 +0530

    gokul and jobin recovered the issues

--- src/main/java/com/healthcare/medical_devices_inventory/model/Device.java
+++ src/main/java/com/healthcare/medical_devices_inventory/model/Device.java
@@ -1,6 +1,17 @@
 package com.healthcare.medical_devices_inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 class Device {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
 
+    private String modelNumber;
+    private String serialNumber;
+    private String 
 }
